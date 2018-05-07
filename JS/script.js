cc9bba47ff1c0b179c2eaed58ee90a719f7376ff
bjcreslin <bjcreslin@gmail.com>
2018-05-07 15:23:49 +0700

    обнова

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,10 @@
-console.log("You are at "+window.location);
+console.log("You are at " + window.location);
+let allMoney = parseFloat(prompt("сколько денег всего имеется?", "0,0"));
+let appleNumber = parseInt(prompt("сколько яблок купили?", "0"));
+let applePrice = parseFloat(prompt("По какой цене яблоки?", "0,0"));
+let carrotNumber = parseInt(prompt("сколько морковок купили?", "0"));
+let carrotPrice = parseFloat(prompt("По какой цене морковки?", "0,0"));
+
+let fullSpend = appleNumber * applePrice + carrotNumber * carrotPrice;
+let isEnough = allMoney >= fullSpend;
+document.body.innerText = isEnough;
